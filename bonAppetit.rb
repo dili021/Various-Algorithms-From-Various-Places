89e302e33fafa555d5afdf9888d3d8cc4e48a68f
Stefan Dili <stefan.dili.021@gmail.com>
2020-05-11 09:38:31 +0200

    Create bonAppetit.rb

---
+++ bonAppetit.rb
@@ -0,0 +1,4 @@
+# [Challenge on hackerrank](https://www.hackerrank.com/challenges/bon-appetit/problem)
+
+# If Anna got overcharged print refund she should receive, else print "Bon Appetit"
+print b > (bill.sum - bill[k]) / 2 ? (b - (bill.sum - bill[k]) / 2) : "Bon Appetit"
